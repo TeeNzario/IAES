0eca5c956cc25618751a188b6d00fa9e77a51806
TeeNzario <teen.2555@gmail.com>
2026-05-07 12:00:11 +0700

    add curriculum and title in db

---
+++ server/prisma/migrations/20260507043323_add_title_to_staff_and_students/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "staff_users" ADD COLUMN     "title" VARCHAR(50);
+
+-- AlterTable
+ALTER TABLE "students" ADD COLUMN     "title" VARCHAR(50);
---
+++ server/prisma/migrations/20260507045255_add_curriculum_id_to_staff_and_students/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "staff_users" ADD COLUMN     "curriculum_id" INTEGER;
+
+-- AlterTable
+ALTER TABLE "students" ADD COLUMN     "curriculum_id" INTEGER;
